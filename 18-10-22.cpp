d8ce950f360dd338284ac5a350f675f921f8e5e8
vivekBoii <yadav100adu@gmail.com>
2022-10-18 20:53:15 +0530

    -

--- 18-10-22.cpp
+++ 18-10-22.cpp
@@ -105,6 +105,38 @@ using namespace std;
         };
 
 /* Printing longest common subsequence */
+    // optimal
+        class Solution {
+        public:
+            int longestCommonSubsequence(string s1, string s2) {
+                int n = s1.size(), m = s2.size();
+                vector<vector<int>> dp(n+1,vector<int> (m+1,0));
+                                    
+                for(int ind1=1;ind1<=n;ind1++){
+                    for(int ind2=1;ind2<=m;ind2++){
+                        if(s1[ind1-1] == s2[ind2-1])
+                            dp[ind1][ind2] = dp[ind1-1][ind2-1] + 1;
+                        else dp[ind1][ind2] = max(dp[ind1-1][ind2],dp[ind1][ind2-1]);
+                    }
+                }
+                int i=n,j=m;
+                string s="";
+                while(i > 0 and j > 0){
+                    if(s1[i] == s2[j]){
+                        s.push_back(s1[i]);
+                        i--,j--;
+                    }else if(dp[i-1][j] > dp[i][j-1]){
+                        i--;
+                    }else{
+                        j--;
+                    }
+                }
+                reverse(s.begin(),s.end());
+                cout<<s<<endl;
+                return dp[n][m];
+            }
+        };
+
 /* Printing longest common subsequence all */
 /* Longest Common Substring 
     1. approach unique
@@ -311,6 +343,95 @@ using namespace std;
         };
 
 // Delete Operation for Two Strings
+    // brute
+        class Solution {
+        public:
+            int pre(int i,int j,string & s1,string & s2){
+                if(i == 0 or j == 0) return 0;
+                
+                if(s1[i-1] == s2[j-1])
+                    return 1 + pre(i-1,j-1,s1,s2);
+                else return max(pre(i-1,j,s1,s2),pre(i,j-1,s1,s2));
+            }
+            int common(string &s1,string &s2){
+                int n = s1.size(),m = s2.size();
+                return pre(n,m,s1,s2);        
+            }
+            int minDistance(string word1, string word2) {
+                return word1.size() + word2.size() - 2*common(word1,word2);   
+            }
+        };
+
+    // memo
+        class Solution {
+        public:
+            int pre(int i,int j,string & s1,string & s2,vector<vector<int>> &dp){
+                if(i == 0 or j == 0) return 0;
+                if(dp[i][j] != -1) return dp[i][j];
+                
+                if(s1[i-1] == s2[j-1])
+                    return dp[i][j] = 1 + pre(i-1,j-1,s1,s2,dp);
+                else return dp[i][j] = max(pre(i-1,j,s1,s2,dp),pre(i,j-1,s1,s2,dp));
+            }
+            int common(string &s1,string &s2){
+                int n = s1.size(),m = s2.size();
+                vector<vector<int>> dp(n+1,vector<int> (m+1,-1));
+                return pre(n,m,s1,s2,dp);        
+            }
+            int minDistance(string word1, string word2) {
+                return word1.size() + word2.size() - 2*common(word1,word2);   
+            }
+        };
+
+    // tab
+        class Solution {
+        public:
+            int common(string &s1,string &s2){
+                int n = s1.size(),m = s2.size();
+                vector<vector<int>> dp(n+1,vector<int> (m+1,0));
+                
+                for(int i=1;i<=n;i++){
+                    for(int j=1;j<=m;j++){
+                        if(s1[i-1] == s2[j-1])
+                            dp[i][j]= 1 + dp[i-1][j-1];
+                        else
+                            dp[i][j]= max(dp[i-1][j],dp[i][j-1]);
+                    }
+                }
+                
+                return dp[n][m];
+            }
+            int minDistance(string word1, string word2) {
+                return word1.size() + word2.size() - 2*common(word1,word2);   
+            }
+        };
+        
+    // optimize
+        class Solution {
+        public:
+            int common(string &s1,string &s2){
+                int n = s1.size(),m = s2.size();
+                vector<int> dp(m+1,0),curr(m+1,0);
+                
+                for(int i=1;i<=n;i++){
+                    for(int j=1;j<=m;j++){
+                        if(s1[i-1] == s2[j-1])
+                            curr[j]= 1 + dp[j-1];
+                        else
+                            curr[j]= max(dp[j],curr[j-1]);
+                    }
+                    dp = curr;
+                }
+                
+                return dp[m];
+            }
+            int minDistance(string word1, string word2) {
+                return word1.size() + word2.size() - 2*common(word1,word2);   
+            }
+        };
+
+/* Shortest Common Supersequence 
+    1. don't know have any aproach*/
     // brute
     // memo
     // tab
